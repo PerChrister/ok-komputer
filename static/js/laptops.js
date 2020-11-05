6a34c95dc93c396cb5350eabada9ff02abfea514
PerChrister <perchrister96@gmail.com>
2020-11-05 16:17:38 +0100

    Fixed JS Stylings

--- static/js/laptops.js
+++ static/js/laptops.js
@@ -1,26 +1,27 @@
 const laptopElement = document.getElementById('container-laptops');
 const orderElement = document.getElementById('container-laptop-order');
 
-function Laptop(name,processor,hdd,ram,description,price){
+function Laptop(name,features,description,price,image){
     this.name = name;
-    this.processor = processor;
-    this.hdd = hdd;
-    this.ram = ram;
+    this.features = features;
     this.description = description;
     this.price = price;
+    this.image = image;
 }
 
 let laptops = [];
 
-const hp = new Laptop('HP Pavilion 15','i7','256 SSD','16 GB RAM','A really good laptop for the dystopia that is coming.','10000 KR');
-const lenovo = new Laptop('Lenovo IdeaPad 330','i5','1 TB HDD','8 GB RAM','This laptop is mostly used by the Karma Police and performs really well. Highly recommended!','13000 KR');
-const asus = new Laptop('Asus VivoBook 14','i5','512 SSD','8 GB RAM','A small yet high performing laptop recommended by Mr. Yorke himself.','15000 KR');
-const acer = new Laptop('Acer Aspire 3','i5','256 SSD','8 GB RAM','The perfect laptop with a good enough portable size and performance perfect for myxomatosis experiments.','12000 KR');
+const hp = new Laptop('HP Pavilion 15','i7, 256 SSD, 16 GB RAM','A really good laptop for the dystopia that is coming.','10000 KR',"../images/hp.jpg");
+const lenovo = new Laptop('Lenovo IdeaPad 330','i5, 1 TB HDD, 8 GB RAM','This laptop is mostly used by the Karma Police and performs really well. Highly recommended!','13000 KR',"../images/lenovo.jpg");
+const asus = new Laptop('Asus VivoBook 14','i5, 512 SSD, 8 GB RAM','A small yet high performing laptop recommended by Mr. Yorke himself.','15000 KR',"../images/asus.jpg");
+const acer = new Laptop('Acer Aspire 3','i5, 256 SSD, 8 GB RAM','The perfect laptop with a good enough portable size and performance perfect for myxomatosis experiments.','12000 KR',"../images/acer.jpg");
 
 laptops.push(hp);
 laptops.push(lenovo);
 laptops.push(asus);
 laptops.push(acer);
+
+console.log(laptops)
 
 const laptopList =  
     `
@@ -28,15 +29,18 @@ const laptopList =
         <p>Laptops</p>
             <div class='js-class'> 
                 <p>
-                    <select id="select-id">
-                        <option>${laptops.name}</option>
+                    <select id='select-id'>
+                    <option>${hp.name}</option>
+                    <option>${lenovo.name}</option>
+                    <option>${asus.name}</option>
+                    <option>${acer.name}</option>
                     </select>
                 </p>
-                <p>Features: <p>
+                <p>Features: <img src='${hp.image}'></img><p>
             </div>
     </div>    
     `;
-
+   
 const order =  
 `
 <div class='order-class'>
